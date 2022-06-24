6b4b0477f56a2629225396ca7a4c75ddd667832e
JoePrinzevalli <joeprinzevalli123@gmail.com>
2022-06-23 20:05:07 -0400

    updating starter files going back to react 18

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,25 @@
-import React from "react";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
-
-// import withContext from './Context';
-import Courses from "./components/Courses";
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
-    
   return (
-    
     <div className="App">
-        <Router>
-          <Routes>
-            <Route path='courses' component={Courses}/>
-          </Routes>
-        </Router>
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
     </div>
   );
 }
-
 
 export default App;
